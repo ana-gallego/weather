f6826bd1bedde92975f03a2eac3ef0a9a5e6bf32
Ana María <anagallego.dev@gmail.com>
2024-04-16 09:04:13 -0500

    Update constants.dart
    chore(constants.dart): update API keys in constants file for security reasons

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,3 @@
-const kOpenWeatherAppId = '26e8f82fffc04649558bcae4ab6d422e';
-const kUnsplashClientId = 'dSbjHCixLrRy_HfZLiZaBimfKr9KFr-0ZjyxouM0-Wc';
-const kPlacesClientApiKey = 'AIzaSyDV1hNyXVsVpuhBYcydrz7oI9Cu3bQA700';
+const kOpenWeatherAppId = 'YOUR_APP_ID';
+const kUnsplashClientId = 'YOUR_CLIENT_ID';
+const kPlacesClientApiKey = 'YOUR_API_KEY';
